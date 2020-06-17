4d661a795b979685633ce9a2694ff45448da3770
Ignacio Ceaglio <nachoceaglio17@gmail.com>
2020-06-17 20:25:52 -0300

    Updates

---
+++ precache-manifest.23c90b26329779ef36b18fa6cb696035.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02dd095888a5974f9fbd1e071c7c8df6",
+    "url": "/weather-app-with-react/index.html"
+  },
+  {
+    "revision": "b5f96e0bfcd02d8c274f",
+    "url": "/weather-app-with-react/static/css/2.7e16aca5.chunk.css"
+  },
+  {
+    "revision": "7c116f99aeccdc72ea16",
+    "url": "/weather-app-with-react/static/css/main.5aab7b5f.chunk.css"
+  },
+  {
+    "revision": "b5f96e0bfcd02d8c274f",
+    "url": "/weather-app-with-react/static/js/2.3c097907.chunk.js"
+  },
+  {
+    "revision": "7c116f99aeccdc72ea16",
+    "url": "/weather-app-with-react/static/js/main.4c43824a.chunk.js"
+  },
+  {
+    "revision": "6db60866e3c5bff57d5b",
+    "url": "/weather-app-with-react/static/js/runtime~main.ba771820.js"
+  },
+  {
+    "revision": "0292f55a680c9013a58efb9a2bb1cc0b",
+    "url": "/weather-app-with-react/static/media/weather-app-background.0292f55a.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app-with-react/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app-with-react/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app-with-react/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app-with-react/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weather-app-with-react/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
